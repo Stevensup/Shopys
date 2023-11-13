7f99f3604ec6e16b6679b95160be1289487c31b4
Stevensup <buseche@unbosque.edu.co>
2023-11-12 19:40:18 -0500

    ajuste login

--- src/main/java/co/edu/unbosque/Controller/Auth/AuthController.java
+++ src/main/java/co/edu/unbosque/Controller/Auth/AuthController.java
@@ -1,17 +1,20 @@
 package co.edu.unbosque.Controller.Auth;
 
+import co.edu.unbosque.Controller.Auth.ClienteLoginRequest;
+import co.edu.unbosque.Model.Cliente;
+import co.edu.unbosque.Service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import co.edu.unbosque.Model.Cliente;
-import co.edu.unbosque.Service.ClienteService;
-
+@Transactional
 @RestController
+@CrossOrigin(origins = { "http://localhost:8081", "http://localhost:8080", "*" })
 @RequestMapping("/login")
 public class AuthController {
 
@@ -19,14 +22,16 @@ public class AuthController {
     private ClienteService clienteService;
 
     @PostMapping
-    public ResponseEntity<String> autenticarCliente(@RequestParam String email, @RequestParam String userPassword) {
+    public ResponseEntity<String> autenticarCliente(@RequestBody ClienteLoginRequest clienteLoginRequest) {
+        String email = clienteLoginRequest.getEmail();
+        String userPassword = clienteLoginRequest.getUserPassword();
+
         Cliente clienteAutenticado = clienteService.autenticarCliente(email, userPassword);
 
         if (clienteAutenticado != null) {
-            return ResponseEntity.status(HttpStatus.OK).body("Sesión iniciada: "+ email); // Autenticación exitosa
+            return ResponseEntity.status(HttpStatus.OK).body("Sesión iniciada: " + email); // Autenticación exitosa
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // Autenticación fallida
         }
     }
 }
-
---
+++ src/main/java/co/edu/unbosque/Controller/Auth/ClienteLoginRequest.java
@@ -0,0 +1,24 @@
+package co.edu.unbosque.Controller.Auth;
+
+public class ClienteLoginRequest {
+    private String email;
+    private String userPassword;
+
+    // Getters y setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
