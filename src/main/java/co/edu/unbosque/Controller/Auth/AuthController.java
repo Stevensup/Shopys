d7a8bd3af369a583bbd57c7c307cafe482c6e445
Stevensup <buseche@unbosque.edu.co>
2023-11-13 22:20:46 -0500

    login bloque ok

--- src/main/java/co/edu/unbosque/Controller/Auth/AuthController.java
+++ src/main/java/co/edu/unbosque/Controller/Auth/AuthController.java
@@ -3,6 +3,8 @@ package co.edu.unbosque.Controller.Auth;
 import co.edu.unbosque.Controller.Auth.ClienteLoginRequest;
 import co.edu.unbosque.Model.Cliente;
 import co.edu.unbosque.Service.ClienteService;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 @Transactional
 @RestController
 @CrossOrigin(origins = { "http://localhost:8081", "http://localhost:8080", "*" })
@@ -20,18 +24,38 @@ public class AuthController {
 
     @Autowired
     private ClienteService clienteService;
+    private Map<String, Integer> intentosFallidos = new HashMap<>();
 
     @PostMapping
-    public ResponseEntity<String> autenticarCliente(@RequestBody ClienteLoginRequest clienteLoginRequest) {
-        String email = clienteLoginRequest.getEmail();
-        String userPassword = clienteLoginRequest.getUserPassword();
+public ResponseEntity<String> autenticarCliente(@RequestBody ClienteLoginRequest clienteLoginRequest) {
+    String email = clienteLoginRequest.getEmail();
+    String userPassword = clienteLoginRequest.getUserPassword();
 
-        Cliente clienteAutenticado = clienteService.autenticarCliente(email, userPassword);
+    Cliente clienteAutenticado = clienteService.autenticarCliente(email, userPassword);
 
-        if (clienteAutenticado != null) {
-            return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"Sesión iniciada: " + email + "\"}");
+    if (clienteAutenticado != null && !clienteAutenticado.isCtaBloqueada()) {
+        // Restablecer el contador de intentos fallidos si la autenticación es exitosa
+        intentosFallidos.remove(email);
+        return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"Sesión iniciada: " + email + "\"}");
+    } else if (clienteAutenticado != null && clienteAutenticado.isCtaBloqueada()) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("La cuenta está bloqueada.");
+    } else {
+        // Incrementar el contador de intentos fallidos
+        intentosFallidos.put(email, intentosFallidos.getOrDefault(email, 0) + 1);
+
+        // Verificar si se superó el límite de intentos fallidos
+        if (intentosFallidos.getOrDefault(email, 0) >= 3) {
+            clienteService.bloquearCliente(email);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("La cuenta está bloqueada.");
         } else {
-             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("usuario o contrasena invalida"); // Autenticación exitosa
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario o contraseña inválida");
         }
     }
 }
+
+}
+
+
+
+
+
